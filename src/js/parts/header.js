504dea1a2be7347a3b0d253ee2a6357283fafc04
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-07-26 20:15:53 +0300

    sg

--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -1,18 +1,27 @@
 export const header = () => {
-    const burger = document.querySelector('.header__burger');
+  const burger = document.querySelector('.header__burger');
 
-    burger.addEventListener('click', openNav);
+  burger.addEventListener('click', openNav);
+
+  window.addEventListener('scroll', function () {
+    const logoCont = document.querySelector('.logo__cont');
+    if (window.scrollY > 50) {
+      logoCont.classList.add('scroled');
+    } else {
+      logoCont.classList.remove('scroled');
+    }
+  });
 };
 
 const openNav = evt => {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const target = evt.currentTarget;
-    const navigation = document.querySelector('.navigation');
+  const target = evt.currentTarget;
+  const navigation = document.querySelector('.navigation');
 
-    target.classList.remove('noAct');
-    navigation.classList.remove('noAct');
-    target.classList.toggle('opened');
-    navigation.classList.toggle('opened');
-    document.querySelector('body').classList.add('overHideMob');
-}
+  target.classList.remove('noAct');
+  navigation.classList.remove('noAct');
+  target.classList.toggle('opened');
+  navigation.classList.toggle('opened');
+  document.querySelector('body').classList.add('overHideMob');
+};
