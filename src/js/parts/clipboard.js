4b5027607ab61ea0560ce00828c257d979ba225b
Uzurch1k <galcenkovlad7@gmail.com>
2024-10-03 16:58:50 +0300

    fix

--- src/js/parts/clipboard.js
+++ src/js/parts/clipboard.js
@@ -1,6 +1,9 @@
 const copyBtns = document.querySelectorAll('.clipboard');
 
-copyBtns.forEach(btn => {
+copyBtns?.forEach(btn => {
+  // Створюємо властивість для зберігання таймера в кожному елементі
+  btn.timer = null;
+
   btn.addEventListener('click', () => {
     const idCopy = btn.dataset.id;
 
@@ -13,9 +16,20 @@ copyBtns.forEach(btn => {
           .writeText(linkText)
           .then(() => {
             btn.classList.add('isCopy');
+
+            // Перезапускаємо таймер, якщо є попередній
+            if (btn.timer) {
+              clearTimeout(btn.timer);
+            }
+
+            // Таймер на 2 секунди для зняття класу
+            btn.timer = setTimeout(() => {
+              btn.classList.remove('isCopy');
+              btn.timer = null;
+            }, 2000);
           })
           .catch(err => {
-            console.error('Ошибка при копировании: ', err);
+            console.error('Помилка при копіюванні: ', err);
           });
       }
     }
