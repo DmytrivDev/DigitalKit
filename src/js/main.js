5bf57e02948a54870948a94b1ae4670be26c7dad
Uzurch1k <galcenkovlad7@gmail.com>
2024-10-03 16:23:03 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
-import { tabs } from "./parts/tabs";
-import { related } from "./parts/related";
-import { main } from "./parts/main";
-import { pointer } from "./parts/pointer";
-import { dividers } from "./parts/dividers";
-import { together } from "./parts/together";
-import { header } from "./parts/header";
-import { toTop } from "./parts/footer";
-import { galCar } from "./parts/accordeon";
-import { team } from "./parts/team";
-import { talent } from "./parts/talent";
+import { tabs } from './parts/tabs';
+import { related } from './parts/related';
+import { main } from './parts/main';
+import { pointer } from './parts/pointer';
+import { dividers } from './parts/dividers';
+import { together } from './parts/together';
+import { header } from './parts/header';
+import { toTop } from './parts/footer';
+import { galCar } from './parts/accordeon';
+import { team } from './parts/team';
+import { talent } from './parts/talent';
+import './parts/clipboard';
 
 header();
 main();
