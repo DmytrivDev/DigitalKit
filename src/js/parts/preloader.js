6443050098c165a3ce64fe406c501748c357f2a8
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-10-07 19:58:16 +0300

    404

--- src/js/parts/preloader.js
+++ src/js/parts/preloader.js
@@ -29,27 +29,19 @@ if (messages) {
       clearTimeout(mouseStopTimeout);
     }
 
-    // Устанавливаем новый таймер на 100ms после остановки мышки
-    mouseStopTimeout = setTimeout(() => {
-      const centerX = window.innerWidth / 2;
-      const centerY = window.innerHeight / 2;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
 
-      messages.forEach((message, index) => {
-        // Рандомно определяем положительное или отрицательное значение
-        const offsetXDirection = Math.random() < 0.5 ? 1 : -1;
-        const offsetYDirection = Math.random() < 0.5 ? 1 : -1;
+    messages.forEach((message, index) => {
+      let offsetX = (currentMousePosition.x - centerX) / 20;
+      let offsetY = (currentMousePosition.y - centerY) / 20;
 
-        // Определяем смещение с учетом направления
-        const offsetX =
-          ((currentMousePosition.x - centerX) / 40) * offsetXDirection + 10;
-        const offsetY =
-          ((currentMousePosition.y - centerY) / 40) * offsetYDirection + 10;
+      if (message.classList.contains('type1')) {
+        offsetX = -(currentMousePosition.x - centerX) / 20;
+        offsetY = -(currentMousePosition.y - centerY) / 20;
+      }
 
-        const animationDuration = Math.random() * 2 + 1; // Время от 1 до 3 секунд
-
-        // message.style.transition = `transform ${animationDuration}s ease-out`;
-        message.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-      });
-    }, 100); // Задержка перед запуском анимации после остановки мышки
+      message.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    });
   });
 }
